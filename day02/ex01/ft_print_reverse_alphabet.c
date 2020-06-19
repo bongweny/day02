7077e521e6ceb4c71d2934a0a569b42797738fb8
Brian Ngwenya <bongweny@c5r2s5.wethinkcode.co.za>
2020-06-19 17:17:10 +0200

    complete

--- day02/ex01/ft_print_reverse_alphabet.c
+++ day02/ex01/ft_print_reverse_alphabet.c
@@ -6,22 +6,31 @@
 /*   By: bongweny <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 11:49:55 by bongweny          #+#    #+#             */
-/*   Updated: 2020/06/19 14:33:19 by bongweny         ###   ########.fr       */
+/*   Updated: 2020/06/19 16:00:34 by bongweny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistdio.h>
+#include <unistd.h>
 
-void ft_putchar(char c)
+void ft_putchar(char c)	
 {
-	write (1, &c, 1);
+	write(1, &c, 1);
 }
-void ft_ print_alphabet(void)
 
+void ft_print_alphabet(void)
 {
 	char letter;
-	letter = 'a';
-	while (letter <= 'z')
-{ ft_putchar(letter);
-   return(0);
-}   
+
+   	letter = 'z'; 
+	while (letter >= 'a')
+	{
+	ft_putchar(letter);
+	letter--;
+	}
+}
+
+int main(void)
+{
+	ft_print_alphabet();
+	return 0;
+}
