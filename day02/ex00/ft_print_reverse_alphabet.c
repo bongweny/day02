3a31ffeaf2d37c38135f5d44f1859e9fd0d85185
Brian Ngwenya <bongweny@c5r2s5.wethinkcode.co.za>
2020-06-25 09:58:00 +0200

    completed

---
+++ day02/ex00/ft_print_reverse_alphabet.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bongweny <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 11:49:55 by bongweny          #+#    #+#             */
+/*   Updated: 2020/06/24 10:23:19 by bongweny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)	
+{
+	write(1, &c, 1);
+}
+
+void ft_print_alphabet(void)
+{
+	char letter;
+
+   	letter = 'z'; 
+	while (letter >= 'a')
+	{
+	ft_putchar(letter);
+	letter--;
+	}
+}
+
+int main(void)
+{
+	ft_print_alphabet();
+	return 0;
+}
--- day02/ex01/ft_print_reverse_alphabet.c
+++ day02/ex01/ft_print_reverse_alphabet.c
@@ -1,31 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bongweny <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/19 11:49:55 by bongweny          #+#    #+#             */
-/*   Updated: 2020/06/19 16:00:34 by bongweny         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-void ft_putchar(char c)	
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void ft_print_alphabet(void)
+void	 ft_print_alphabet(void)
 {
 	char letter;
 
-   	letter = 'z'; 
-	while (letter >= 'a')
+	letter = 'a';
+	while (letter <= 'z')
 	{
 	ft_putchar(letter);
-	letter--;
+	letter++;
 	}
 }
 
